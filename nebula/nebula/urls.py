bb1396369339c1e302d7d2f00cbd28141669e9c5
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-10-13 12:38:52 -0500

    app

--- nebula/nebula/urls.py
+++ nebula/nebula/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for storefront project.
+URL configuration for nebula project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
